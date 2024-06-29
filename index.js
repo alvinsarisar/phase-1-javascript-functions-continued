1c43cb45a603f7cf79b1d27992e07f81998138cc
alvin <alvin.sarisar@student.moringaschool.com>
2024-06-29 22:41:57 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// Defines the saturdayFun function with a default activity
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Defines the mondayWork function with a default activity
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Defines the wrapAdjective function which returns a function to wrap an adjective
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
